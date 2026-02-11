1f6516c6727e6ac252f12d9bbebbfc0f2bc0518a
MemcacheRaspberry <memcache@raspberry.com>
2026-02-11 22:56:52 +0100

    Added two solid MCP tools

--- src/mcp/server.py
+++ src/mcp/server.py
@@ -2,10 +2,12 @@
 import logging
 
 from mcp.server.fastmcp import FastMCP
+from tools.device_info import register_config_tools
 from tools.device_status import register_tools as register_device_status_tools
 from tools.router_tools import register_tools as register_router_tools
 from tools.health_router import register_health_tools
 from tools.topology_router import register_topology_tools
+from tools.interface_router import register_interface_tools
 
 # ----------------- LOGGING -----------------
 logging.basicConfig(
@@ -22,6 +24,8 @@ register_router_tools(mcp)
 register_device_status_tools(mcp)
 register_health_tools(mcp)
 register_topology_tools(mcp)
+register_config_tools(mcp)
+register_interface_tools(mcp)
 
 # ----------------- RUN SERVER -----------------
 if __name__ == "__main__":
